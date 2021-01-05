560a24408db45ea7b808652fe06cbfb3e9d46e8d
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-05 22:03:16 +0530

    Initial Commit

---
+++ UnSplash App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnSplash App
+//
+//  Created by Mahesh Prasad on 05/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnSplash App/UnSplash_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSplash_AppApp.swift
+//  UnSplash App
+//
+//  Created by Mahesh Prasad on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct UnSplash_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
